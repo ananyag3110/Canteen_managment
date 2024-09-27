a7118479373cd01c2e2a76d7f11c4c1a231bf260
Ananya G <ananyaprasad3110@gmail.com>
2024-09-27 18:54:16 +0530

    Uploading the folder

---
+++ gat/gat_canteen/src/App.js
@@ -0,0 +1,13 @@
+import AppRoutes from "./AppRoutes.js";
+import Header from "./components/Header/Header.js";
+
+function App() {
+  return (
+  <>
+    <Header />
+    <AppRoutes/>
+  </>
+  );
+}
+
+export default App;
